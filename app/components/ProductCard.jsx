4780712b408d8859faa306f6679c5a610d4dbdf1
slyderyg <kprokofev88@icloud.com>
2023-09-13 11:09:04 +0300

    created product card

---
+++ app/components/ProductCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const ProductCard = () => {
+  return (
+    <>
+    <Box>
+
+    </Box>
+    </>
+  )
+}
+
+export default ProductCard
